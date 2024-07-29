5866d94e18e7fb5892104806c7821b2276a7a59d
EzgiDuzenlier <duzenlierezgi@gmail.com>
2024-07-29 16:10:32 +0300

    mapping

---
+++ RabbitMQFakeStore/Publisher/Mapping/MapProfile.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using AutoMapper;
+using Shared.RequestResponseMessages;
+using Shared.RequestViewModel;
+
+namespace Publisher.Mapping
+{
+	public class MapProfile : Profile
+	{
+		public MapProfile()
+		{
+			CreateMap<RequestMessage, CreateProductVM>().ReverseMap();
+		}
+	}
+}
+
